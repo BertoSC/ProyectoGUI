d55903e912b2817c2954814fe4b009d5df0bdd35
a23albertogc <a23albertogc@iessanclemente.net>
2024-12-09 14:11:48 +0100

    ass

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ import java.sql.*;
 public class Main {
     public static void main(String[] args) throws SQLException {
         //Connection con = DriverManager.getConnection("jdbc:sqlite:L:\\DesenvolvementoInterfaces\\CentroMedico");
-        Connection con = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\VSPC-BLACKFRIDAY\\Desktop\\DI\\centromedico");
+        Connection con = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\a23albertogc\\Desktop\\BARBERIA\\Barber");
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
